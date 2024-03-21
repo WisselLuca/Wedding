aa938d3041b54f0a71bf18a8fb3e59d394a3c411
Luca Wissel <luca.wissel@enel.com>
2024-03-21 13:49:47 +0100

    working copy

--- src/components/working/working.js
+++ src/components/working/working.js
@@ -13,6 +13,7 @@ const Working = () => {
 
     const envelope = useRef();
     const partecipatzioneFront = useRef();
+    const rotatingPart = useRef();
 
     /* useEffect(() => {
          gsap.fromTo(envelope.current,
@@ -41,9 +42,12 @@ const Working = () => {
              })
      }, [])
  */
+    const vh = (coef) => window.innerHeight * (coef / 100);
+    const vw = (coef) => window.innerWidth * (coef / 100);
+
 
     useGSAP(() => {
-        const animation = gsap.fromTo(".envelope",
+        const animationEnvelope = gsap.fromTo(".envelope",
             { height: '100%' },
             {
                 height: 0,
@@ -52,32 +56,82 @@ const Working = () => {
                 stagger: 0.7
             }
         )
+        const animationRotate = gsap.to(".rotating_part",
+            {
+                rotation: "180",
+                transformOrigin: '50% 100%'
+            }
+        )
 
+
+        //sbustare
         ScrollTrigger.create({
-            trigger: ".envelopeCointainer",
-            start: "top 40%",
-            end: "bottom 50%",
+            trigger: ".scroll_element",
+            start: vh(100) + " 40%",
+            end: vh(140) + " 50%",
             markers: true,
-            animation: animation,
-            scrub: true
+            animation: animationEnvelope,
+            scrub: true,
         })
 
+        //pinn dell'immagine
+        ScrollTrigger.create({
+            trigger: '.scroll_element',
+            start: vh(100) + " 10%",
+            endTrigger: '',
+            end: 'bottom top',
+            pin: '.envelopeCointainer'
+        })
+
+        gsap.fromTo("#linear img", {scale:0.1}, {scale:0.4, duration: 10, ease: "none", repeat: -1});
+
+        //ruota la cosa sotto
+        ScrollTrigger.create({
+            trigger: ".scroll_element",
+            start: vh(140) + 10 + " 50%",
+            end: vh(160) + " 50%",
+            markers: true,
+            animation: animationRotate,
+            scrub: true,
+        })
 
     })
 
     return (
         <div className='App'>
-            <div className='divSpace-working'>Welcome</div>
-            <div className='box-container-working'>
-                <div className="envelopeCointainer">
-                    <div ref={partecipatzioneFront} className="partecipazioneFront"> {/*orange*/}
-                        <div ref={envelope} className="envelope">{/* green */}
-                        </div>
-                    </div>
-
+            <div className="page_container" style={{ width: '100%', height: '100vh' }}>
+                <div className='scroll_element' style={{ backgroundColor: '#ffdab9' }}>
+                </div>
+                <div style={{ flex: 1, backgroundColor: '#faebd7' }}>
+                </div>
+                <div style={{ flex: 1, backgroundColor: '#00ff00' }}>
+                    <div className='divSpace-working'>Welcome</div>
+                    <div className='box-container-working'>
+                        <div className="envelopeCointainer">
+                            <div ref={partecipatzioneFront} className="partecipazione_front">
+                                <div ref={envelope} className="envelope">
+                                </div>
+                                <div ref={rotatingPart} className="rotating_part">
+                                </div>
+                            </div>
+                        </div >
+                    </div >
+                </div>
+                <div style={{ flex: 1, backgroundColor: '#faebd7' }}>
                 </div>
             </div>
+            {/*<div className='divSpace-working'>Welcome</div>
+            <div className='box-container-working'>
+                <div className="envelopeCointainer">
+                    <div ref={partecipatzioneFront} className="partecipazione_front"> 
+            <div ref={envelope} className="envelope">
+            </div>
+            <div ref={rotatingPart} className="rotating_part">
+            </div>
         </div>
+                </div >
+            </div >*/}
+        </div >
     )
 }
 
