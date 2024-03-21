431cac88ed6517a95678f2c1e290892f53795312
Luca Wissel <luca.wissel@enel.com>
2024-03-21 12:28:44 +0100

    test

--- src/components/working/working.js
+++ src/components/working/working.js
@@ -13,6 +13,7 @@ const Working = () => {
 
     const envelope = useRef();
     const partecipatzioneFront = useRef();
+    const rotatingPart = useRef();
 
     /* useEffect(() => {
          gsap.fromTo(envelope.current,
@@ -53,31 +54,74 @@ const Working = () => {
             }
         )
 
+        let envelopeDisappear = gsap.timeline();
+        envelopeDisappear.fromTo(".envelope",
+            { height: '80%' },
+            {
+                height: 0,
+                ease: "none",
+                duration: 1000,
+                stagger: 0.7
+            }
+        )
         ScrollTrigger.create({
-            trigger: ".envelopeCointainer",
+            animation: envelopeDisappear,
+            trigger: ".scroll_element",
             start: "top 40%",
-            end: "bottom 50%",
+            end: "12% 50%",
+            scrub: 1,
             markers: true,
-            animation: animation,
-            scrub: true
-        })
+        });
+
+
+
+        /* ScrollTrigger.create({
+             trigger: ".envelopeCointainer",
+             start: "top 40%",
+             end: "bottom 50%",
+             markers: true,
+             animation: animation,
+             scrub: true,
+         })*/
 
 
     })
 
     return (
         <div className='App'>
-            <div className='divSpace-working'>Welcome</div>
-            <div className='box-container-working'>
-                <div className="envelopeCointainer">
-                    <div ref={partecipatzioneFront} className="partecipazioneFront"> {/*orange*/}
-                        <div ref={envelope} className="envelope">{/* green */}
-                        </div>
-                    </div>
-
+            <div className="page_container" style={{ width: '100%', height: '100vh' }}>
+                <div className='scroll_element' style={{ backgroundColor: '#ffdab9' }}>
+                </div>
+                <div style={{ flex: 1, backgroundColor: '#faebd7' }}>
+                </div>
+                <div style={{ flex: 1, backgroundColor: '#00ff00' }}>
+                    <div className='divSpace-working'>Welcome</div>
+                    <div className='box-container-working'>
+                        <div className="envelopeCointainer">
+                            <div ref={partecipatzioneFront} className="partecipazione_front">
+                                <div ref={envelope} className="envelope">
+                                </div>
+                                <div ref={rotatingPart} className="rotating_part">
+                                </div>
+                            </div>
+                        </div >
+                    </div >
+                </div>
+                <div style={{ flex: 1, backgroundColor: '#faebd7' }}>
                 </div>
             </div>
+            {/*<div className='divSpace-working'>Welcome</div>
+            <div className='box-container-working'>
+                <div className="envelopeCointainer">
+                    <div ref={partecipatzioneFront} className="partecipazione_front"> 
+            <div ref={envelope} className="envelope">
+            </div>
+            <div ref={rotatingPart} className="rotating_part">
+            </div>
         </div>
+                </div >
+            </div >*/}
+        </div >
     )
 }
 
